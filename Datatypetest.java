cb78471fb16f717c351dc771f96eca0311c1bb9a
Nandhunnk <133625747+Nandhunnk@users.noreply.github.com>
2023-05-17 10:09:31 +0530

    Add files via upload
    Datatypetest.java

---
+++ Datatypetest.java
@@ -0,0 +1,40 @@
+package com.sbi;
+
+public class Datatypetest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		byte nurroll=120;
+		short collroll=30000;
+		int univroll=2147483647;
+		long intnlroll=9223372036854775807L;
+		
+		float si=3.55f;
+		double ci=56.23;
+		
+		System.out.println("Byte    		:"+nurroll);
+		System.out.println("Size of Byte	:"+Byte.SIZE);
+		System.out.println("");
+		System.out.println("Short    		:"+collroll);
+		System.out.println("Size of Byte	:"+Short.SIZE);
+		System.out.println("");
+		System.out.println("Integer    		:"+univroll);
+		System.out.println("Size of Integer	:"+Integer.SIZE);
+		System.out.println("");
+		System.out.println("Long    		:"+intnlroll);
+		System.out.println("Size of Long	:"+Long.SIZE);
+		System.out.println("");
+		System.out.println("Float    		:"+si);
+		System.out.println("Size of Float	:"+Float.SIZE);
+		System.out.println("");
+		System.out.println("Double    		:"+ci);
+		System.out.println("Size of Double	:"+Double.SIZE);
+		System.out.println("");
+		String city="Navi Mumbai";
+		String bankname="State bank of India";
+		System.out.println("");
+		System.out.println("City    		:"+city);
+		System.out.println("Bank Name    		:"+bankname);
+	}
+
+}
